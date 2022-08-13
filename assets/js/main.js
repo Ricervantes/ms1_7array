815a400c35912e660f395eaa9ad0c87b3d5bddb9
Ricervantes <racervantes.dwfsc9@ucamp.utel.edu.mx>
2022-08-13 11:31:44 -0500

    string templates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,43 @@
 console.log("Entro al main.js");
-var nombre="juan";
- console.log(nombre);
+
+
+let nombre_persona="juan";
+ //console.log(nombre);
  
 
- let nombre="manuel";
+ /*let nombre="manuel";
  console.log(nombre);
 
  let nombre="juan";
- console.log(nombre);
+ console.log(nombre);*/
+ let nombre=`juan`;
+ let apellido="perez";
+
+ console.log(`mi nombre es:${nombre},mi apellido es:${apellido}`);
+ 
+
+ //objetos en javaScript
+let array_perro=["willy","negro"];
+console.log(array_perro[1]);
+
+
+ let perro=    {
+    "nombre":"willy",
+    "color":"negro",
+    "raza":"labrador",
+    "edad":4,
+    "paises":8,
+
+ };
+ console.log("el nombre es :",perro.nombre);
+ console.log("la raza es :",perro.raza);
+ console.log(perro["color"]);
+
+ let keys= object.keys(perro);
+ console.log(keys);
+ keys.forEach(llave => {
+    console.log(perro[llave]);
+    console.log(llave);
+
+ });
+
